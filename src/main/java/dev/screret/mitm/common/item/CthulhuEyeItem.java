f32b66aa7dd5ab2d3c1da03d83ce314cf1c33779
screret <68943070+screret@users.noreply.github.com>
2025-11-28 20:17:49 +0200

    clean up CthulhuEyeItem a bit

--- src/main/java/dev/screret/mitm/common/item/CthulhuEyeItem.java
+++ src/main/java/dev/screret/mitm/common/item/CthulhuEyeItem.java
@@ -3,7 +3,9 @@ package dev.screret.mitm.common.item;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
@@ -21,48 +23,50 @@ public class CthulhuEyeItem extends Item {
 
     @Override
     public InteractionResult useOn(UseOnContext context) {
-        if (MITMConfig.Server.enableQthulhuEyeConversion.get()) {
-            var player = context.getPlayer();
-            var level = context.getLevel();
-            BlockPos blockpos = context.getClickedPos();
-            if (!level.mayInteract(player, blockpos)) {
-                return InteractionResult.PASS;
-            }
-
-            for (var block : EyeConversionManager.INSTANCE.getAllConversions()) {
-                var originalState = level.getBlockState(blockpos);
-                if (block.getValue().test(originalState)) {
-                    copyProperties(level, blockpos, block.getKey().defaultBlockState(), originalState);
-                    if (!level.isClientSide) {
-                        var serverLevel = (ServerLevel) level;
-                        var random = level.random;
-                        var pos = new Vec3(blockpos.getX() + 0.5D, blockpos.getY() + 0.5D, blockpos.getZ() + 0.5D);
-                        serverLevel.sendParticles(ParticleTypes.END_ROD,
-                                pos.x,
-                                pos.y,
-                                pos.z,
-                                32,
-                                random.nextDouble(),
-                                random.nextDouble(),
-                                random.nextDouble(),
-                                0.5D
-                        );
-                        serverLevel.sendParticles(ParticleTypes.TOTEM_OF_UNDYING,
-                                pos.x,
-                                pos.y,
-                                pos.z,
-                                32,
-                                random.nextDouble(),
-                                random.nextDouble(),
-                                random.nextDouble(),
-                                0.5D
-                        );
-                    }
-                    return InteractionResult.sidedSuccess(level.isClientSide);
-                }
-            }
-
+        if (!MITMConfig.Server.enableQthulhuEyeConversion.get()) {
+            return super.useOn(context);
         }
+        Level level = context.getLevel();
+        if (!(level instanceof ServerLevel serverLevel)) {
+            return InteractionResult.SUCCESS;
+        }
+
+        Player player = context.getPlayer();
+        BlockPos blockpos = context.getClickedPos();
+        if (!level.mayInteract(player, blockpos)) {
+            return InteractionResult.PASS;
+        }
+        for (var conversionEntry : EyeConversionManager.INSTANCE.getAllConversions()) {
+            BlockState originalState = level.getBlockState(blockpos);
+            if (!conversionEntry.getValue().test(originalState)) {
+                continue;
+            }
+            copyProperties(level, blockpos, conversionEntry.getKey().defaultBlockState(), originalState);
+            RandomSource random = level.random;
+            Vec3 pos = new Vec3(blockpos.getX() + 0.5D, blockpos.getY() + 0.5D, blockpos.getZ() + 0.5D);
+            serverLevel.sendParticles(ParticleTypes.END_ROD,
+                    pos.x,
+                    pos.y,
+                    pos.z,
+                    32,
+                    random.nextDouble(),
+                    random.nextDouble(),
+                    random.nextDouble(),
+                    0.5D
+            );
+            serverLevel.sendParticles(ParticleTypes.TOTEM_OF_UNDYING,
+                    pos.x,
+                    pos.y,
+                    pos.z,
+                    32,
+                    random.nextDouble(),
+                    random.nextDouble(),
+                    random.nextDouble(),
+                    0.5D
+            );
+            return InteractionResult.CONSUME;
+        }
+
         return super.useOn(context);
     }
 
