a565bda394c202c7027361f8cb78a8e75ea97117
screret <68943070+screret@users.noreply.github.com>
2025-12-14 20:39:53 +0200

    register mui packets (oops)

--- ModularUI/src/main/java/dev/screret/modularui/network/NetworkHandler.java
+++ ModularUI/src/main/java/dev/screret/modularui/network/NetworkHandler.java
@@ -1,3 +1,25 @@
 package dev.screret.modularui.network;
 
-public class NetworkHandler {}
+import dev.screret.modularui.ModularUI;
+import dev.screret.modularui.network.packets.OpenGuiPacket;
+import dev.screret.modularui.network.packets.SyncHandlerPacket;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+
+@EventBusSubscriber(modid = ModularUI.MOD_ID)
+public class NetworkHandler {
+
+    public static final String NETWORK_VERSION = "1.0.0";
+
+    @SubscribeEvent
+    public static void registerPayloads(RegisterPayloadHandlersEvent event) {
+        PayloadRegistrar registrar = event.registrar(NETWORK_VERSION);
+        // spotless:off
+        registrar.playBidirectional(OpenGuiPacket.TYPE, OpenGuiPacket.CODEC, OpenGuiPacket::execute);
+        registrar.playBidirectional(SyncHandlerPacket.TYPE, SyncHandlerPacket.CODEC, SyncHandlerPacket::execute);
+        // spotless:on
+    }
+}
