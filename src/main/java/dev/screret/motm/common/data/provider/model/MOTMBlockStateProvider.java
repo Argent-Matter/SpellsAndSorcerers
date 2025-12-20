2e09635798c63602931a4f3bb02751add4778226
screret <68943070+screret@users.noreply.github.com>
2025-12-21 01:08:43 +0200

    port stone model

--- src/main/java/dev/screret/motm/common/data/provider/model/MOTMBlockStateProvider.java
+++ src/main/java/dev/screret/motm/common/data/provider/model/MOTMBlockStateProvider.java
@@ -2,12 +2,14 @@ package dev.screret.motm.common.data.provider.model;
 
 import dev.screret.motm.MOTMUtil;
 import dev.screret.motm.MagicOfTheMind;
+import dev.screret.motm.common.block.PortStoneBlock;
 import dev.screret.motm.data.MOTMBlocks;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
 import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.client.model.generators.ModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -22,7 +24,16 @@ public class MOTMBlockStateProvider extends BlockStateProvider {
     protected void registerStatesAndModels() {
         simpleBlockWithItem(MOTMBlocks.GLINT_ORE.get());
         simpleBlockWithItem(MOTMBlocks.SOULSTEEL_BLOCK.get());
-        simpleBlockWithItem(MOTMBlocks.PALANTIR.get(), getUncheckedFile(MOTMUtil.id("palantir")));
+        simpleBlockWithItem(MOTMBlocks.MEMORYSTONE.get());
+        simpleBlockWithItem(MOTMBlocks.UNAWAKENED_MEMORYSTONE.get());
+
+        getVariantBuilder(MOTMBlocks.PORT_STONE.get())
+                .forAllStatesExcept(state -> {
+                    PortStoneBlock.Part part = state.getValue(PortStoneBlock.PART);
+                    return ConfiguredModel.builder()
+                            .modelFile(getExistingFile(MOTMUtil.id("port_stone_" + part.getSerializedName())))
+                            .build();
+                }, PortStoneBlock.TRIGGERED, PortStoneBlock.WATERLOGGED);
     }
 
     public void simpleBlockWithItem(Block block) {
