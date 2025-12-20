325e8a9be9cfd85b824b565960007a9e718b8b94
screret <68943070+screret@users.noreply.github.com>
2025-12-20 14:54:29 +0200

    only add a POI to the bottom block of port stones (instead of all 4 blocks)

--- src/main/java/dev/screret/motm/data/MOTMPoiTypes.java
+++ src/main/java/dev/screret/motm/data/MOTMPoiTypes.java
@@ -1,6 +1,7 @@
 package dev.screret.motm.data;
 
 import dev.screret.motm.MagicOfTheMind;
+import dev.screret.motm.common.block.PortStoneBlock;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
@@ -9,25 +10,36 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import com.google.common.collect.ImmutableSet;
-
+import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class MOTMPoiTypes {
 
     // spotless:off
     public static final DeferredRegister<PoiType> POINT_OF_INTEREST_TYPES = DeferredRegister.create(BuiltInRegistries.POINT_OF_INTEREST_TYPE, MagicOfTheMind.MOD_ID);
 
-    public static final DeferredHolder<PoiType, PoiType> PORT_STONE = POINT_OF_INTEREST_TYPES.register("port_stone", () -> new PoiType(getBlockStates(MOTMBlocks.PORT_STONE), 0, 1));
+    public static final DeferredHolder<PoiType, PoiType> PORT_STONE = POINT_OF_INTEREST_TYPES.register("port_stone",
+            () -> new PoiType(getBlockStates(MOTMBlocks.PORT_STONE, state -> state.getValue(PortStoneBlock.PART) == PortStoneBlock.Part.BOTTOM), 0, 1));
 
     // spotless:on
 
     private static Set<BlockState> getBlockStates(Block block) {
-        return ImmutableSet.copyOf(block.getStateDefinition().getPossibleStates());
+        // no need to care about immutability, the contents are copied to another set in PoiType.new anyway.
+        return new HashSet<>(block.getStateDefinition().getPossibleStates());
     }
 
     private static Set<BlockState> getBlockStates(Supplier<? extends Block> block) {
         return getBlockStates(block.get());
+    }
+
+    private static Set<BlockState> getBlockStates(Block block, Predicate<BlockState> filter) {
+        return block.getStateDefinition().getPossibleStates().stream().filter(filter).collect(Collectors.toSet());
+    }
+
+    private static Set<BlockState> getBlockStates(Supplier<? extends Block> block, Predicate<BlockState> filter) {
+        return getBlockStates(block.get(), filter);
     }
 }
