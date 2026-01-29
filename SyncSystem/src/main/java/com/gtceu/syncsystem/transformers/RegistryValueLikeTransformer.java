38ab913c922def27e493449225084f94afae81eb
screret <68943070+screret@users.noreply.github.com>
2026-01-29 18:29:19 +0200

    rename ResourceLocationReferenceTransformer to RegistryValueLikeTransformer and add more sanity checks

---
+++ SyncSystem/src/main/java/com/gtceu/syncsystem/transformers/RegistryValueLikeTransformer.java
@@ -0,0 +1,60 @@
+package com.gtceu.syncsystem.transformers;
+
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+
+import com.gtceu.syncsystem.SyncSystem;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+public class RegistryValueLikeTransformer<T> implements ValueTransformer<T> {
+
+    private static final String NOT_A_REGISTRY = "[non-registry lookup]";
+
+    private final Function<T, @Nullable ResourceLocation> getId;
+    private final Function<ResourceLocation, @Nullable T> getValue;
+    private final @Nullable String registryName;
+
+    public RegistryValueLikeTransformer(Function<T, @Nullable ResourceLocation> getId,
+                                        Function<ResourceLocation, @Nullable T> getValue) {
+        this.getId = getId;
+        this.getValue = getValue;
+        this.registryName = NOT_A_REGISTRY;
+    }
+
+    public RegistryValueLikeTransformer(Registry<T> registry) {
+        this.getId = registry::getKey;
+        this.getValue = registry::get;
+        this.registryName = registry.key().location().toString();
+    }
+
+    @Override
+    public Tag serializeNBT(T value, ValueTransformer.TransformerContext<T> context) {
+        ResourceLocation id = getId.apply(value);
+        if (id == null) {
+            SyncSystem.LOGGER.error("Unknown value in registry {}: {}", this.registryName, value);
+            return new CompoundTag();
+        }
+        return StringTag.valueOf(id.toString());
+    }
+
+    @Override
+    public @Nullable T deserializeNBT(Tag tag, ValueTransformer.TransformerContext<T> context) {
+        ResourceLocation id = ResourceLocation
+                .tryParse(ValueTransformer.assertTagType(StringTag.class, tag, context).getAsString());
+        if (id == null) {
+            SyncSystem.LOGGER.error("Invalid registry key {}", tag.getAsString());
+            return null;
+        }
+        T value = getValue.apply(id);
+        if (value == null) {
+            SyncSystem.LOGGER.error("Unknown key in registry {}: {}", this.registryName, id);
+        }
+        return getValue.apply(id);
+    }
+}
